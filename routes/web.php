b9e3b1cd13a07dff9162dc203176f1975868ef71
Nymul Islam <nymulislam.dev@gmail.com>
2025-07-12 23:30:33 +0600

    mastering

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('admin.dashboard');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
 
